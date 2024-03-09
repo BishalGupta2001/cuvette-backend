3512564d283ff7cc28d6c3c5a8e9f6142f2432de
Bishal Gupta <guptabishal512@gmail.comgit config --global user.name Bishal>
2024-03-09 11:58:23 +0530

    ejs completed ch-2

--- BackendCuvette/NodeServer-1/index.js
+++ BackendCuvette/NodeServer-1/index.js
@@ -20,66 +20,114 @@
 // this is using express framework
 const express = require('express') // this line means we have imorted express
 const app = express() // this line means creating a instance of a function
+const bodyParser = require('body-parser')
+
+// learning middleware  
+// ye basically bich ma se response de deta hai 
+
+const isLoggedIn = (req, res, next) => {
+    let loggedIn = true;
+    if (loggedIn) {
+        next(); 
+    } else {
+        res.json({
+            message: "Please logg in You are not Logged in"
+        })
+    }
+}
+
+const isPremium = (req, res, next)=>{
+    let premium = true;
+    if(premium){
+        next()
+    } else {
+        res.json({
+            message : "You are not a premium user buy premium for this shit"
+        })
+    }
+}
+
+// .use is for attaching the middlewear
+// .use() applies middleware to all the routes 
+app.use(bodyParser.urlencoded());
+app.use(isLoggedIn);
+app.use(isPremium);
 
 app.get('/', function (req, res) {
-    res.send('Hello World finally its working &')
+    res.send('Hello World working in node Server 1')
 })
 
-app.get('/human', function (req, res) {
+app.get('/register', isLoggedIn , function (req, res) {
+    res.sendFile(__dirname + '/register.html')
+})
+
+
+app.get('/register/successful',isLoggedIn, function (req, res) {
+    res.send("congrat it has been submited")
+})
+
+
+app.post('/api/register', function (req, res) {
+    console.log(req.body);
+    res.redirect("/register/successful");
+})
+
+
+app.get('/human',isLoggedIn , isPremium,function (req, res) {
     const human = [
         {
             name: 'Anant',
             age: '23',
             height: '5.4',
-            img : "https://reqres.in/img/faces/1-image.jpg",
+            img: "https://reqres.in/img/faces/1-image.jpg",
         },
         {
             name: 'Bishal',
             age: '22',
             height: '5.5',
-            img : "https://reqres.in/img/faces/4-image.jpg",
+            img: "https://reqres.in/img/faces/4-image.jpg",
         },
         {
             name: 'Priyanshu',
             age: '24',
             height: '5.6',
-            img : "https://reqres.in/img/faces/3-image.jpg",
+            img: "https://reqres.in/img/faces/3-image.jpg",
         },
         {
             name: 'Anushka',
             age: '25',
-            height: '5.2', 
-            img : "https://reqres.in/img/faces/2-image.jpg",
+            height: '5.2',
+            img: "https://reqres.in/img/faces/2-image.jpg",
         },
     ]
     console.log(human);
     res.send(human);
 })
-app.get('/animal', function(req, res){
+app.get('/animal',isLoggedIn , isPremium, function (req, res) {
     const animal = [
         {
-            name : 'Lion',
-            color : 'golden',
-             age: '35',
+            name: 'Lion',
+            color: 'golden',
+            age: '35',
         },
         {
-            name : 'Tiger',
-            color : 'Black & golden',
-             age: '55',
+            name: 'Tiger',
+            color: 'Black & golden',
+            age: '55',
         },
         {
-            name : 'Fox',
-            color : 'Black',
-             age: '23',
+            name: 'Fox',
+            color: 'Black',
+            age: '23',
         },
     ]
     console.log(animal);
     res.send(animal);
 })
 
-app.get('/pet' , function(req, res){
-    res.sendFile( __dirname+ '/index.html');
+app.get('/pet', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 })
 app.listen(4000, () => {
-    console.log("server chalu che");
+    console.log("server running at NodeServer 1");
 })
--- BackendCuvette/NodeServer-2/index.js
+++ BackendCuvette/NodeServer-2/index.js
@@ -2,39 +2,66 @@ const express = require('express') // this line means we have imorted express
 const app = express() // this line means creating a instance of a function
 const ejs = require('ejs');// imported ejs
 
+// this is array of object of data
+const USERS = [{
+
+    username: 'george',
+    name: 'George blutch',
+    email: 'george.bluth@reqres.in',
+    profileImgUrl: 'https://reqres.in/img/faces/1-image.jpg',
+    likeCoding: false
+},
+{
+    username: 'jannet',
+    name: 'jannet weaver ',
+    email: 'jannet.weaver@reqres.in',
+    profileImgUrl: 'https://reqres.in/img/faces/2-image.jpg',
+    likeCoding: true
+},
+]
 
 // as we are using ejs we have to inform our browser so we will set
 // as our view engine is ejs so app.set view engine is ejs ;
-app.set('view engine ' , 'ejs');
 
-app.get('/', (req, res)=> {
-    res.send('Hello World finally its working &')
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('Hello World finally Node server 2 is working ')
 })
 
-app.get('/user/george', (req, res)=> {
-    res.render('users.ejs', {
-        name : 'George  blutch',
-        email:'george.bluth@reqres.in',
-        profileImgUrl:'https://reqres.in/img/faces/1-image.jpg',
-        likeCoding : false
-    })
+app.get('/user/:username', (req, res) => {
+    const { username } = req.params
+    const userDetails = USERS.find((user) => user.username === username)
+    res.render('users.ejs', userDetails);
+    // console.error(`User with username '${username}' not found`);
+    res.status(404).send('there is some error User not found ');
+
+
 })
 
-app.get('/user/jannet', (req, res)=> {
-    res.render('users.ejs', {
-        name : 'jannet weaver ',
-        email:'jannet.weaver@reqres.in',
-        profileImgUrl:'https://reqres.in/img/faces/2-image.jpg',
-        likeCoding : true
-    })
-})
+// isme v pura cure nahi hua hume bar abar ek hi chiz ko banana/likhna pad arha hai so now we will learn about ROUTING PARAM
 
-app.get('/user/jannet',  (req, res) => {
-    res.render('users.ejs')
-})
+// app.get('/user/george', (req, res) => {
+//     res.render('users.ejs', {
+//         name: 'George  blutch',
+//         email: 'george.bluth@reqres.in',
+//         profileImgUrl: 'https://reqres.in/img/faces/1-image.jpg',
+//         likeCoding: false
+//     })
+// })
 
+// app.get('/user/jannet', (req, res) => {
+//     res.render('users.ejs', {
+// name: 'jannet weaver ',
+// email: 'jannet.weaver@reqres.in',
+// profileImgUrl: 'https://reqres.in/img/faces/2-image.jpg',
+// likeCoding: true
+//     })
+// })
 
-
+// app.get('/user/jannet', (req, res) => {
+//     res.render('users.ejs')
+// })
 
 // bina templete engines ka bana rahe the toh bar bar html file banana pad raha tha   kuch aise 
 // app.get('/user/george', (req, res)=> {
@@ -45,9 +72,7 @@ app.get('/user/jannet',  (req, res) => {
 //     res.sendFile(__dirname + '/jannet.html')
 // })
 
-//  now we will use EJS  jo ki dyanmic rahega now we don't have to create multiple html file por a particular user 
-
-
+//  now we will use EJS  jo ki dyanmic rahega now we don't have to create multiple html file f  or a particular user 
 
 app.listen(4000, () => {
     console.log("server chalu che -2 ");
